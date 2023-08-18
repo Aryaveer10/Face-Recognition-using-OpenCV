c7d05ceb1fc8b52c19a0919ee8d5690fbf7043b1
Aryaveer Sadal <109890838+Aryaveer10@users.noreply.github.com>
2023-08-18 23:25:01 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,62 @@
+import cv2
+import numpy as np
+import face_recognition
+import os
+from datetime import datetime
+path = 'Train_Images'
+images = []
+className = []
+myList = os.listdir(path)
+print(myList)
+for c1 in myList:
+ curImg = cv2.imread(f'{path}/{c1}')
+ images.append(curImg)
+ className.append(os.path.splitext(c1)[0])
+print(className)
+def findEncodings(images):
+ encodeList = []
+ for img in images:
+ img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+ encode = face_recognition.face_encodings(img)[0]
+ encodeList.append(encode)
+ return encodeList
+def markAttendance(name):
+ with open('Attendance.csv','r+') as f:
+ myDataList = f.readlines()
+ nameList = []
+ for line in myDataList:
+ entry = line.split(',')
+ nameList.append(entry[0])
+ if name not in nameList:
+ now = datetime.now()
+ dtString = now.strftime('%H:%M:%S')
+ f.writelines(f'\n{name},{dtString}')
+print('Starting Encoding...')
+encodeListKnow = findEncodings(images)
+print('Encoding Completed')
+cap = cv2.VideoCapture("Test_Images/Test5.mp4")
+while True:
+ success, img = cap.read()
+ imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+ imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+ facesCurFrame = face_recognition.face_locations(imgS)
+ encodeCurFrame = face_recognition.face_encodings(imgS, facesCurFrame)
+ for encodeFace, faceLoc in zip(encodeCurFrame, facesCurFrame):
+ matches = face_recognition.compare_faces(encodeListKnow, encodeFace)
+ faceDis = face_recognition.face_distance(encodeListKnow, encodeFace)
+ #print(faceDis)
+ matchIndex = np.argmin(faceDis)
+ if matches[matchIndex]:
+ name = className[matchIndex].upper()
+ #print(name)
+ y1, x2, y2, x1 = faceLoc
+ y1, x2, y2, x1 = y1*4, x2*4, y2*4, x1*4
+ cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+ cv2.rectangle(img, (x1, y2-35), (x2, y2), (0, 255, 0), cv2.FILLED)
+ cv2.putText(img, name, (x1+6, y2-6), cv2.FONT_HERSHEY_COMPLEX, 0.75, 
+(255,255,255), 2)
+ markAttendance(name)
+ im = cv2.resize(img, (540, 540))
+ cv2.imshow('Video', im)
+ if cv2.waitKey(1) & 0xFF == ord("q"):
+ break
